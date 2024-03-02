a41417d9ceed3ed40f244b801d4515a2ed989199
heman <rivero@protonmail.com>
2024-03-01 22:30:06 -0800

    first commit

---
+++ dictionaries.py
@@ -0,0 +1,94 @@
+# Dictionaries
+band = {
+    "vocals" : "Plant",
+    "guitar" : "Page"
+}
+
+band2 = dict(vocals="Plant", guitar="Page")
+
+print(band)
+print(band2)
+
+print(type(band))
+print(len(band))
+
+
+# Access items
+print(band["vocals"])
+print(band.get("guitar"))
+
+# List all keys
+print(band.keys())
+
+# List all values
+print(band.values())
+
+# List of key/value pairs as tuples
+print(band.items())
+
+# Verify a key exists
+print("guitar" in band)
+print("rectangle" in band)
+
+# Change values
+band["vocals"] = "Coverdale"
+band.update({"bass" : "JPJ"})
+print(band)
+
+# remove items
+print(band.pop("bass"))
+print(band)
+
+band["drums"] = "Bonham"
+print(band)
+
+print(band.popitem()) #tuple
+print(band)
+
+# Delete and clear
+band["drums"] = "Bonham"
+del band["drums"]
+print(band)
+
+band2.clear()
+print(band2)
+
+del band2
+
+# Copy dictionaries
+
+# band2 = band # create a reference, same place in memory, not a copy
+# print("Bad copy!")
+# print(band2)
+# print(band)
+
+# band2["drums"] = "Dave"
+# print(band)
+
+band2 = band.copy()
+band2["drums"] = "Dave"
+print("Good copy!")
+print(band)
+print(band2)
+
+# or use the dict() constructor fucntion
+band3 = dict(band)
+print("Good copy!")
+print(band3)
+
+# Nested dictionaries
+member1 = {
+    "name": "Plant",
+    "instrument": "vocals"
+}
+member2 = {
+    "name": "Page",
+    "instrument": "guitar"
+}
+band = {
+    "member1": member1, # careful not to quote the value of the key, it is a reference
+    "member2": member2
+}
+print(band)
+
+print(band["member1"])
