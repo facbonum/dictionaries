eee0589238fd69b68df71872ab12d8c84559265d
heman <rivero@protonmail.com>
2024-03-03 14:39:42 -0800

    exercises

--- dictionaries.py
+++ dictionaries.py
@@ -94,6 +94,7 @@ print(band)
 print(band["member1"]["name"])
 
 # Sets
+# setname. <-- set name followed by dot notation pulls up all available methods
 
 nums = {1, 2, 3, 4}
 
@@ -103,3 +104,45 @@ print(nums)
 print(nums2)
 print(type(nums))
 print(len(nums))
+
+# No duplicate allowed
+nums = {1, 2, 2, 3}
+print(nums)
+
+# True is a dupe of 1, False is a dupe of zero
+nums = {1, True, 2, False, 3, 4, 0}
+print(nums)
+
+# Check if a value is in a set
+print(2 in nums)
+# but you cannot refer to an element in the set with an index position or key
+
+# Add a new element to a set
+nums.add(8)
+print(nums)
+
+# Add elements from one set to another
+morenums = {5, 6, 7}
+nums.update(morenums)
+print(nums)
+
+# You can use update with lists, tuples, and dictionaries, too.
+
+# Merge two sets to create a new set
+one = {1, 2, 3}
+two = {5, 6, 7}
+
+mynewset = one.union(two)
+print(mynewset)
+
+# Keep only the duplicates
+one = {1, 2, 3}
+two = {2, 3, 4}
+one.intersection_update(two)
+print(one)
+
+# Keep everything except the duplicates
+one = {1, 2, 3}
+two = {2, 3, 4}
+one.symmetric_difference_update(two)
+print(one)
